84fdb876de8813bc813856144c9523468d4d5374
dottg0n20 <dev@dariocaruso.info>
2022-03-27 16:38:06 +0200

    ref

--- test/testvar.js
+++ test/testvar.js
@@ -1,4 +1,7 @@
-export const variable = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.variable = void 0;
+exports.variable = {
     pippo: 'baudo',
 };
 //# sourceMappingURL=testvar.js.map
